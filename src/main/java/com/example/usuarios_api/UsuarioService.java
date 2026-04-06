ca5983286fb270514d2608b8ffa9ddb34556d3eb
evellyn severo <evellyn.severo@zup.com.br>
2026-04-06 02:27:35 -0300

    feat: implementação de regras de negócio para cadastro, busca, listagem e deletes de usuários

--- src/main/java/com/example/usuarios_api/UsuarioService.java
+++ src/main/java/com/example/usuarios_api/UsuarioService.java
@@ -1,4 +1,49 @@
 package com.example.usuarios_api;
 
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.ResourceAccessException;
+
+import java.util.List;
+
+@Service
 public class UsuarioService {
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public UsuarioResponse criar(UsuarioRequest request) {
+        if (usuarioRepository.existsByEmail(request.getEmail())) {
+            throw new IllegalArgumentException("E-mail já cadastrado:" + request.getEmail());
+        }
+        Usuario usuario = new Usuario();
+        usuario.setNome(request.getNome());
+        usuario.setEmail(request.getEmail());
+        Usuario salvo = usuarioRepository.save(usuario);
+        return new UsuarioResponse(salvo.getId(), salvo.getNome(), salvo.getEmail());
+    }
+
+    public List<UsuarioResponse> listarTodos() {
+        return usuarioRepository.findAll().stream()
+                .map(u -> new UsuarioResponse(u.getId(), u.getNome(), u.getEmail()))
+                .toList();
+    }
+
+    public UsuarioResponse buscarId(Long id) {
+        Usuario usuario = usuarioRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Usuário ID" + id + "não encontrado"));
+        return new UsuarioResponse(usuario.getId(), usuario.getNome(),
+                usuario.getEmail());
+    }
+
+    public void deletar(Long id) {
+        if (!usuarioRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Usuário ID " + id + " não encontrado");
+        }
+        usuarioRepository.deleteById(id);
+
+    }
+
 }
